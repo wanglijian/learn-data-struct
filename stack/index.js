5625fe727892ce59f78002304927c11f5eb660bd
77944998@qq.com <77944998@qq.co>
2024-01-21 20:22:40 +0800

    feat: 增加栈结构的应用-十进制转二进制

--- stack/index.js
+++ stack/index.js
@@ -1,24 +1,22 @@
 class Stack {
-    constructor() {
-        this.items = []
-    }
+    #items = []
     push(item) {
-        this.items.push(item)
+        this.#items.push(item)
     }
     pop() {
-        return this.items.pop()
+        return this.#items.pop()
     }
     peak() {
-        return this.items[this.items.length - 1]
+        return this.#items.at(-1)
     }
 
     isEmpty() {
-        return this.items.length === 0
+        return this.#items.length === 0
     }
     size() {
-        return this.items.length
+        return this.#items.length
     }
     toString(split = '') {
-        return this.items.join(split)
+        return this.#items.join(split)
     }
 }
